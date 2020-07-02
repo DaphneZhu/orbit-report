04082cf45ca7ab90c146e56c4fd3a3310d6febf3
DaphneZhu <61849400+DaphneZhu@users.noreply.github.com>
2020-07-02 04:58:56 -0500

    the EventRepositoryInterface is created

--- Desktop/coding-events-persistent-controller/src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ Desktop/coding-events-persistent-controller/src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,15 +1,21 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class EventCategory {
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     public EventCategory (int id, String name) {
         this.id = id;
         this.name = name;
     }
+
+    public EventCategory () {}
 
     public int getId() {
         return id;
