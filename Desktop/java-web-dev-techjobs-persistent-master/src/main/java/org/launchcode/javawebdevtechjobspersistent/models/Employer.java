7f8d840763fa5e943357b9234aedc8de7fcea7f6
DaphneZhu <61849400+DaphneZhu@users.noreply.github.com>
2020-07-05 06:13:13 -0500

    added the field for location and with validation and setter and getter in Employer model class

--- Desktop/java-web-dev-techjobs-persistent-master/src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ Desktop/java-web-dev-techjobs-persistent-master/src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+
 public class Employer extends AbstractEntity {
 
+    @NotEmpty
+    @Max(120)
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
